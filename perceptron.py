c7e7590fff6761b082d6dad93c4f23dd2c1f9df0
LonelyDreamer005 <vinay2020x15.10@gmail.com>
2026-04-27 12:43:43 +0530

    percep update

--- perceptron.py
+++ perceptron.py
@@ -28,3 +28,15 @@ print('Bias', model.intercept_)
 
 
 
+from sklearn.datasets import load_iris 
+from sklearn.linear_model import Perceptron 
+from sklearn.metrics import accuracy_score 
+data = load_iris() 
+X = data.data 
+y = data.target 
+model = Perceptron() 
+model.fit(X, y) 
+y_pred = model.predict(X) 
+print("Accuracy:", accuracy_score(y, y_pred)) 
+print("Weights:", model.coef_) 
+print("Bias:", model.intercept_)

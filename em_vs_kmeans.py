6623f381166f1a9982ad02d8886d26a227edc079
LonelyDreamer005 <vinay2020x15.10@gmail.com>
2026-04-27 12:35:50 +0530

    updated emvskm

--- em_vs_kmeans.py
+++ em_vs_kmeans.py
@@ -34,3 +34,27 @@ plt.scatter(x_scaled[:,0], x_scaled[:,1], c=em_labels)
 plt.title('EM')
 plt.show()
 
+
+
+from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import silhouette_score
+import matplotlib.pyplot as plt
+data = load_iris()
+x = data.data
+x = StandardScaler().fit_transform(x)
+k = KMeans(n_clusters=3)
+k_labels = k.fit_predict(x)
+g = GaussianMixture(n_components = 3)
+g_labels = g.fit_predict(x)
+print("KMeans:", silhouette_score(x, k_labels))
+print("EM:", silhouette_score(x, g_labels))
+plt.subplot(1,2,1)
+plt.scatter(x[:,0], x[:,1], c=k_labels)
+plt.title("KMeans")
+plt.subplot(1,2,2)
+plt.scatter(x[:,0], x[:,1], c=g_labels)
+plt.title("EM")
+plt.show()
